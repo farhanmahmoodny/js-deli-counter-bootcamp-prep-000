3ce01a24f004eaa2982eb6cffc0ae9c8e24317ab
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-26 01:19:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(lineOfCustormers, newPerson){
   var newList = []
   for( var i = 0; i < listOfCustomers.length; i++){
     var people = listOfCustomers[i]
-    newList += people 
+    newList += people
   }
   newList += newPerson
   return 'Welcome, ' + newPerson + +'. You are number ' + (newList.indexOf(newPerson) + 1) +' in line.'
