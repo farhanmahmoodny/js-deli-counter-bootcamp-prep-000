3aa6c70136f4c5bdae6dc41372c4572fcaf9af0a
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-26 01:09:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function takeANumber(newPerson){
 var currentCustomers = []
 currentCustomers.push(newPerson)
 for(var i = newPerson; i < currentCustomers.length; i++ ){
-console.log('Welcome, ' + currentCustomers[i] + '. You are number ' + (currentCustomers[i].indexof + 1) + ' in line.')
+console.log('Welcome, ' + currentCustomers[i] + '. You are number ' + (indexOf(currentCustomers[i]) + 1) + ' in line.')
 }
 }
