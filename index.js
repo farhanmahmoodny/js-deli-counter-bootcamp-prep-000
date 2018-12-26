b46e74d54a771485bb7a366fed56a8357e18c450
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-26 01:26:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
 var katzDeliLine = []
-function takeANumber(katzDeliLine, newPerson){
-  var newList = []
-  for( var i = 0; i < katzDeliLine.length; i++){
-    var people = katzDeliLine[i]
-    newList += people
-  }
-  newList += newPerson
-  return 'Welcome, ' + newList.slice(0, newList.lemgth-1) + '. You are number ' + (newList.indexOf(newPerson) + 1) +' in line.'
+// function takeANumber(katzDeliLine, newPerson){
+//   var newList = []
+//   for( var i = 0; i < katzDeliLine.length; i++){
+//     var people = katzDeliLine[i]
+//     newList += people
+//   }
+//   newList += newPerson
+//   return 'Welcome, ' + newList.slice(0, newList.lemgth-1) + '. You are number ' + (newList.indexOf(newPerson) + 1) +' in line.'
+// }
+
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.'
 }
