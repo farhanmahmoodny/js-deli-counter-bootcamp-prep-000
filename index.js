532228b10caa65b83a555457bdf836e9f66f9954
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-26 02:19:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(katzDeliLine){
   else {
   return 'Currently serving ' + katzDeliLine[0] + '.'
 }
-return katzDeliLine.shift()
+katzDeliLine.shift()
 }
 
 function currentLine(katzDeliLine){
