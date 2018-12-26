280f11c05ea699c9cbfcd8115931b6a688e58def
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-26 00:50:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 
 function takeANumber(currentNumber, newPerson){
   var katzDeliLine = []
-  newPerson = ''
   katzDeliLine.push(newPerson)
   currentNumber = 0
   var number = currentNumber + 1
   for (var i = 0; i < katzDeliLine.length; i++){
-  return 'Welcome, ' + katzDeliLine[i] + '. You are number ' + number + ' in line.'
+  return 'Welcome, ' + newPerson + '. You are number ' + number + ' in line.'
 }
 }
