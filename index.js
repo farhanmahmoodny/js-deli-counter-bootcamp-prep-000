48adc92c58343ea47e9d848165f3af1dc4410f83
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-26 00:27:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function takeANumber(currentNumber, newPerson){
+  for (var i = currentNumber+1; i > 0; i++){
+    console.log('Welcome, ' + newPerson + '. You are number ' + i + ' in line.')
+  }
+}
