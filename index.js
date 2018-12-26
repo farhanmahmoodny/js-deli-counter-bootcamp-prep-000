541f43582819bf7bcc2a9477c6409a31de92e3bd
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-26 02:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function nowServing(katzDeliLine){
     return 'There is nobody waiting to be served!'
   }
   else {
-    katzDeliLine.shift()
   return 'Currently serving ' + katzDeliLine[0] + '.'
 }
+katzDeliLine.shift()
 }
 
 function currentLine(katzDeliLine){
