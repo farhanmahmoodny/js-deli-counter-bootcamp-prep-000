6a8c50de8d9badbb726281d520926b492e02eddf
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-26 01:08:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function takeANumber(newPerson){
-currentCustomers = []
+var currentCustomers = []
 currentCustomers.push(newPerson)
 for(var i = newPerson; i < currentCustomers.length; i++ ){
 console.log('Welcome, ' + currentCustomer[i] + '. You are number ' + (currentCustomer[i].indexOf + 1) + ' in line.')
